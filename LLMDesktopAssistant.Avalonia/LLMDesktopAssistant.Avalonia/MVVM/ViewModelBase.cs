e12a03ba29030687bd20497bf0a4e1abb2440f5b
Roman Kondrat'ev <62770895+RomeCore@users.noreply.github.com>
2026-04-12 16:15:46 +0500

    A

---
+++ LLMDesktopAssistant.Avalonia/LLMDesktopAssistant.Avalonia/MVVM/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia.Threading;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LLMDesktopAssistant.Core.MVVM
+{
+	/// <summary>
+	/// Base class for view models. Implements INotifyPropertyChanged to facilitate data binding.
+	/// </summary>
+	public class ViewModelBase : NotifyPropertyChanged
+	{
+		protected static void InvokeUI(Action action, CancellationToken cancellationToken = default)
+		{
+			Dispatcher.UIThread.Invoke(action, DispatcherPriority.Default, cancellationToken);
+		}
+
+		protected static async Task InvokeUIAsync(Action action, CancellationToken cancellationToken = default)
+		{
+			await Dispatcher.UIThread.InvokeAsync(action, DispatcherPriority.Default, cancellationToken);
+		}
+	}
+}
---
+++ LLMDesktopAssistant.Avalonia/LLMDesktopAssistant.Avalonia/MVVM/ViewModelForAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LLMDesktopAssistant.Core.MVVM
+{
+	/// <summary>
+	/// Specifies the view associated with a ViewModel.
+	/// Apply this attribute to a ViewModel class to indicate which view it is associated with.
+	/// </summary>
+	/// <param name="targetView">The type of the view to bind this ViewModel to.</param>
+	[AttributeUsage(AttributeTargets.Class)]
+	public sealed class ViewModelForAttribute(Type targetView) : Attribute
+	{
+		public Type TargetView { get; } = targetView;
+	}
+}
