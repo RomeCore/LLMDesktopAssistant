13e7af3bb2088b52711d3ded9142ee6794ddb02c
Roman Kondrat'ev <62770895+RomeCore@users.noreply.github.com>
2026-04-14 14:23:59 +0500

    Add global using for System.Threading.Tasks

--- LLMDesktopAssistant.Avalonia/LLMDesktopAssistant.Avalonia/GlobalUsings.cs
+++ LLMDesktopAssistant.Avalonia/LLMDesktopAssistant.Avalonia/GlobalUsings.cs
@@ -5,4 +5,5 @@ global using System.Collections.Generic;
 global using System.Collections.Immutable;
 global using System.Linq;
 global using System.Threading;
+global using System.Threading.Tasks;
 global using System.Windows.Input;
