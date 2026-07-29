cd4d856f55338932f7f3ea332fe08ebcab62ff98
Roman Kondrat'ev <62770895+RomeCore@users.noreply.github.com>
2026-07-29 13:11:31 +0500

    feat: add JsonToMarkdownConverter

---
+++ LLMDesktopAssistant/Converters/JsonToMarkdownConverter.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+using System.Text.Json.Nodes;
+using Avalonia.Data.Converters;
+using LLMDesktopAssistant.LLM.Messages;
+using LLMDesktopAssistant.Utils;
+
+namespace LLMDesktopAssistant.Converters
+{
+	public class JsonToMarkdownConverter : IValueConverter
+	{
+		public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			if (value is JsonNode node)
+			{
+				try
+				{
+					return ToolCallArgumentFormatter.FormatToMarkdown(node);
+				}
+				catch
+				{
+					return $"```json\n{node.ToJsonString()}\n```";
+				}
+			}
+			else if (value is string json)
+			{
+				try
+				{
+					node = TolerantJsonParser.Parse(json)!;
+					return ToolCallArgumentFormatter.FormatToMarkdown(node);
+				}
+				catch
+				{
+					return $"```json\n{json}\n```";
+				}
+			}
+			return value;
+		}
+
+		public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
